912c6f11e51ccb62e06fa29a6c68ac80a9696eac
VitAndrGuid <vitor.guidorizzi@hotmail.com>
2025-08-06 13:53:17 -0400

    hello world

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-teal-500 to-indigo-600 text-white">
+    <h1 class="text-5xl font-bold mb-4">Hello, Vue + TypeScript + UnoCSS 👋</h1>
+    <p class="text-lg">Powered by Vite & deployed to GitHub Pages 🚀</p>
+  </main>
+</template>
